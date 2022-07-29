9b5f6c4e0aa5b0d1a21207070b6fcf8962d79e0e
Joan <jfabregatserra@gmail.com>
2022-07-29 15:06:48 +0200

    Update streamlit_app.py
    bg

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,9 +36,9 @@ streamlit.dataframe(fruityvice_normalized)
 
 
 
-# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-# my_cur = my_cnx.cursor()
-# my_cur.execute("SELECT * FROM fruit_load_list;")
-# my_data_row = my_cur.fetchall()
-# streamlit.text("The list:")
-# streamlit.dataframe(my_data_row)
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * FROM fruit_load_list;")
+my_data_row = my_cur.fetchall()
+streamlit.text("The list:")
+streamlit.dataframe(my_data_row)
