62c9013435f6faac75816ca2f3708147b59b4115
Joan <jfabregatserra@gmail.com>
2022-07-29 11:55:18 +0200

    Update streamlit_app.py
    add data entry capabilities

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+# Input veggie section:
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 #Display fruityvice response
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get('https://www.fruityvice.com/api/fruit/watermelon')
